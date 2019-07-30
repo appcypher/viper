8d4ce3caf5e31b2afc392cbcc575ebebd37ee6cc
Stephen Akinyemi <appcypher@outlook.com>
2019-07-30 01:16:24 +0100

    Add documentation to Lexer code

--- compiler/lexer.py
+++ compiler/lexer.py
@@ -1,102 +1,145 @@
+""" Contains Viper's lexer implementation """
+
 from enum import Enum
 
 
 class ValidTokens:
-    identifier_char = [
-
-    ]
+    """
+    Contains some of the legal characters, tokens and keywords allowed by the
+    Lexer
+    """
     keywords = [
         'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await',
         'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',
         'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is',
-        'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return',
-        'try', 'while', 'with', 'yield',
-        'const', 'ref', 'ptr', 'val', 'match', 'let', 'var', 'enum', 'true',
-        'false', 'interface', 'where'
+        'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try',
+        'while', 'with', 'yield', 'const', 'ref', 'ptr', 'val', 'match', 'let',
+        'var', 'enum', 'true', 'false', 'interface', 'where', 'macro'
     ]
 
 
+class Indentation:
+    """ """
+    def __init__(self):
+        self.indent_count = 0
+        self.is_space = True
+        self.indent_factor = 4
+
+
 class Token:
+    """ Token is a unit extracted """
     def __init__(self, data, kind, row, column):
         self.data = data
         self.kind = kind
         self.row = row
         self.column = column
 
+    def __repr__(self):
+        return (f'Token(data="{self.data}", kind={self.kind}, row={self.row}'
+                f', column={self.column})')
+
+    def __eq__(self, other):
+        return (self.data == other.data and self.kind == other.kind
+                and self.row == other.row and self.column == other.column)
+
 
 class TokenKind(Enum):
-    IDENTIFIER = 1
+    """ The different kinds of token """
+    IDENTIFIER = 0
     NEWLINE = 1
     INTEGER_DEC = 2
     FLOAT_DEC = 3
 
 
 class LexerError(Exception):
-    def __init__(self, message, parser):
+    """ Represents the error the lexer can raise """
+    def __init__(self, message, row, column):
         super().__init__(message)
-        self.row = parser.row
-        self.column = parser.column
+        self.row = row
+        self.column = column
 
 
 class Lexer:
+    """ Contains the lexer implementation """
     def __init__(self, code):
         self.code = code
         self.code_length = len(code)
         self.cursor = -1
-        self.row = -1
+        self.row = 0
         self.column = -1
-        self.indentation = 0
+        self.indentation = Indentation()
         self.valid_tokens = ValidTokens
 
     def eat_char(self):
-        # Check if there is a character to be consumed.
-        if self.cursor >= self.code_length:
-            return None
+        """
+        Returns the next character in code and advances the cursor
+        position
+        """
+        if self.cursor + 1 < self.code_length:
+            self.cursor += 1
+            return self.code[self.cursor]
 
-        self.cursor += 1
-        return self.code[self.cursor]
+        return None
 
     def peek_char(self):
-        # Check if there is a character to be consumed.
-        if self.cursor >= self.code_length:
-            return None
+        """ Peeks at the next character in code """
+        if self.cursor + 1 < self.code_length:
+            return self.code[self.cursor + 1]
 
-        return self.code[self.cursor + 1]
+        return None
 
-    def is_identifier(char):
+    def is_identifier_start(char):
+        """ Checks if a character has the following pattern: [A-Za-z_] """
         num = ord(char)
-        return (
-            (64 < num < 91) or   # A-Z
-            (96 < num < 123) or  # a-z
-            (47 < num < 58)      # 0-9
-            (char == '_')        # _
-        )
+        return ((64 < num < 91) or  # A-Z
+                (96 < num < 123) or  # a-z
+                (char == '_')  # _
+                )
+
+    def is_identifier_mid(char):
+        """ Checks if a character has the following pattern: [A-Za-z0-9_] """
+        num = ord(char)
+        return ((64 < num < 91) or  # A-Z
+                (96 < num < 123) or  # a-z
+                (47 < num < 58) or  # 0-9
+                (char == '_')  # _
+                )
 
     def lex(self):
+        """ Breaks code string into tokens that the parser can digest """
         char = self.eat_char()
         tokens = []
 
+        # Loops through each character in the code.
         while char:
-            if char == '\r':
+            row, column = self.row, self.column
+
+            # We check each character in the code and categorize it
+            if char == '\r':  # NEWLINE
                 if self.peek_char() == '\n':
                     self.eat_char()
-                tokens.append(Token('', TokenKind.NEWLINE, self))
 
-            elif char == '\n':
-                tokens.append(Token('', TokenKind.NEWLINE, self))
+                tokens.append(Token('', TokenKind.NEWLINE, row, column))
 
-            elif Lexer.is_identifier(char):
+            elif char == '\n':  # NEWLINE
+                tokens.append(Token('', TokenKind.NEWLINE, row, column))
+
+            elif Lexer.is_identifier_start(char):  # IDENTIFIER
                 token = char
+                peeked_char = self.peek_char()
 
-                while Lexer.is_identifier(self.peek_char()):
-                    token.append(self.eat_char())
+                while peeked_char and Lexer.is_identifier_mid(peeked_char):
+                    token += self.eat_char()
 
-                tokens.append(Token(token, TokenKind.IDENTIFIER, self))
+                    # Peek at the next character in code.
+                    peeked_char = self.peek_char()
 
+                tokens.append(Token(token, TokenKind.IDENTIFIER, row, column))
+
+            # elif ...:  # ...
+            # print('>>>', peeked_char)
+
+            # Consume the next character in code.
             char = self.eat_char()
 
         return tokens
-
-    def lex_identifier(): pass
-
-    def lex_integer_dec(): pass
