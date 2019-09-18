85c4fcd393d7fb56ce8e0f0a96f90a7f5bfaab43
Stephen Akinyemi <appcypher@outlook.com>
2019-09-18 11:38:27 +0100

    Fix buildd script to allow symlink edge case

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # Create a symlink of bin/viper.py at /usr/local/bin/viper
 
 # Get the absolute path of where script is running from
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd)"
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+script_dir="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 script_path="$script_dir/run.sh"
 
 # RETURN VARIABLE
